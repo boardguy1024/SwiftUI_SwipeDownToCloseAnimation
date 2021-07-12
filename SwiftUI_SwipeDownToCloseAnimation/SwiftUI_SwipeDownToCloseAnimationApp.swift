425d10b6f46f6df143d05b7191ee3959c27211b0
boardguy1024 <boardguy1024@gmail.com>
2021-07-13 00:20:36 +0900

    Initial Commit

---
+++ SwiftUI_SwipeDownToCloseAnimation/SwiftUI_SwipeDownToCloseAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwipeDownToCloseAnimationApp.swift
+//  SwiftUI_SwipeDownToCloseAnimation
+//
+//  Created by park kyung seok on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwipeDownToCloseAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
